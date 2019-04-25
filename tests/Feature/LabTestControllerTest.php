e5850eaa951317aa11e4fea0def62d7287db8c00
James Allen <jacques2186@yahoo.com>
2019-04-24 20:32:57 -0400

    test: added lab test controller test

---
+++ tests/Feature/LabTestControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Auth;
+use Tests\TestCase;
+use App\LabTest;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LabTestControllerTest extends TestCase
+{
+    use WithoutMiddleware, RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @test
+     */
+    public function it_should_return_all_lab_tests_when_accessing_tests_route()
+    {
+
+        factory('App\Module', 5)->create()->each(function($u){
+            $u->labtests()->save(factory('App\LabTest')->make());
+        });
+
+        $tests = LabTest::all();
+        $response = $this->json('GET', '/api/tests');
+
+        $response->assertStatus(200)->assertExactJson($tests->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_able_to_store_tests() {
+        $module = factory('App\Module')->create();
+        $newTest = factory('App\LabTest')->make(['module_id'=>$module->id]);
+
+        
+       $response = $this->json('POST', '/api/tests', $newTest->toArray());
+
+       $response->assertStatus(201);
+    }
+}
